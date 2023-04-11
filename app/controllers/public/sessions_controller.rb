b8a1c7a18f3ccafdcd79ab6f9f49251e6027ed57
EC2 Default User <ec2-user@ip-172-31-16-150.ap-northeast-1.compute.internal>
2023-04-11 05:42:18 +0000

    [Add] withdraw修正

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -7,9 +7,10 @@ class Public::SessionsController < Devise::SessionsController
   protected
   def customer_state
     @customer = Customer.find_by(email: params[:customer][:email])
-    return if !@customer
-    if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == false)
+    if @customer
+     if @customer.valid_password?(params[:customer][:password]) && (!@customer.is_deleted == false)
       redirect_to new_customer_registration_path
+     end
     end
   end
   
