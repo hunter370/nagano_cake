9cd1bfb887d4df3b988b801c7223cc3f38c16e95
EC2 Default User <ec2-user@ip-172-31-16-150.ap-northeast-1.compute.internal>
2023-04-15 09:28:32 +0000

    [Edit] admin_genre

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -2,13 +2,13 @@ class Admin::GenresController < ApplicationController
   
   def create
     @genre = Genre.new(genre_params)
-    @genre.admin_id = currrent_admin.id
     @genre.save!
     redirect_to admin_genres_path
   end
   
   def index
     @genre = Genre.new
+    @genres = Genre.all
   end
 
   def edit
