1482fed087a4ac352bb589e3532af0c052237385
EC2 Default User <ec2-user@ip-172-31-16-150.ap-northeast-1.compute.internal>
2023-04-25 07:30:43 +0000

    [Edit] cart_view

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,14 +7,26 @@ class Public::CartItemsController < ApplicationController
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
     @cart_item.item_id = cart_item_params[:item_id]
-    @cart_item.save!
-    redirect_to cart_items_path
+    if CartItem.find_by(item_id: params[:cart_item][:item_id]).present?
+      @cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
+      @cart_item.amount += params[:cart_item][:amount].to_i
+      @cart_item.save!
+      redirect_to cart_items_path
+    elsif @cart_item.save!
+      @cart_items = CartItem.all
+      render :index
+    else
+      render :index
+    end
   end
   
   def update
     @cart_item = CartItem.find(params[:id])
-    @cart_item.update(cart_item_params)
-    redirect_to cart_items_path
+    if @cart_item.update(cart_item_params)
+      redirect_to cart_items_path
+    else
+      render :index
+    end
   end
   
   private
