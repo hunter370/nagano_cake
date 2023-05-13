c8bedc14f65ea4efe7cebeaa6b64c282ee116288
EC2 Default User <ec2-user@ip-172-31-16-150.ap-northeast-1.compute.internal>
2023-05-13 08:41:53 +0000

    [edit] test

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -10,10 +10,9 @@ class Public::RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
   end
   
-  #def after_sign_up_path_for(resource)
-    
-    #マイページへ遷移する(マイページ作成後)
-  #end
+  def after_sign_up_path_for(resource)
+    customers_show_path
+  end
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
